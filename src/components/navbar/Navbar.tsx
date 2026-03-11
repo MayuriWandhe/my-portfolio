edafd5e6cfc13e0bc4e5d2fcb8bfd34bb1589cc6
instaDev-mayuri <mayuri@instaacoders.com>
2026-03-11 14:29:17 +0530

    navbar updates for sm screen

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsopen] = useState(false);
+
   return (
         <>
             <nav className="bg-[#0f172a] fixed w-full z-20 top-0 start-0 border-default">
@@ -8,11 +11,12 @@ const Navbar = () => {
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <span className='text-2xl font-bold whitespace-nowrap bg-gradient-to-r from-indigo-400 to-purple-400 to-pink-400 bg-clip-text text-transparent'>Mayuri Wandhe</span>
                 </a>
-                <button data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-dropdown" aria-expanded="false">
+                <button data-collapse-toggle="navbar-dropdown" type="button" onClick={() => setIsopen(!isOpen)}
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-dropdown" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
                 </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+                <div className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`} id="navbar-dropdown">
                 <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
                     <li>
                     <a href="#about" className="block py-2 px-3 text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0" aria-current="page">About</a>
